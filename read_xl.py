af80084ca1ea2b87dd9211a207ba5efb707b0b91
foy4748 <faisaljfcl@gmail.com>
2021-03-15 17:58:00 +0600

    Functioning -- Testing requires

--- read_xl.py
+++ read_xl.py
@@ -1,34 +1,25 @@
+
 from openpyxl import load_workbook as lw
 from datetime import datetime
-# Attendance.xlsx
-
 try: 
-    from openpyxl.cell import get_column_letter
+    from openpyxl.cell import get_column_letter, column_index_from_string
 except ImportError:
-    from openpyxl.utils import get_column_letter
+    from openpyxl.utils import get_column_letter, column_index_from_string
+# End of Importing
 
-wb = lw('Attendance.xlsx', data_only= True)
 
-#Getting sheet names
-sheets = wb.sheetnames
-print(sheets)
+wb = lw('Attendance.xlsx', data_only= True)	#Reading the Workbook
+sheets = wb.sheetnames 					#Getting sheet names
+w_sh = wb[sheets[0]]					#Reading the First sheet
 
-w_sh = wb[sheets[0]]
-
-#for i in range(1,7):
-#	print(w_sh.cell(row=1,column=i).value , get_column_letter(i))
+fh = open('./store.txt')
 
 collection = list()
-fh = open('./store.txt')
 i = 2
-#print(w_sh.cell(row=i,column=2).value)
 while w_sh.cell(row=i,column=2).value != None:
 	collection.append( (str(w_sh.cell(row=i,column=2).value.strip()), str(w_sh.cell(row=i,column=6).value.strip())) )
-	#fh.write(str(w_sh.cell(row=i,column=2).value) + str(w_sh.cell(row=i,column=6).value) + '\n')
 	i = i + 1
-	
-
-
+#Collection of students constructed
 
 
 #Checking Attended Pupils
@@ -38,43 +29,72 @@ for line in fh:
 	line = line.strip()
 	
 	
-	A = line.split('-')
-	for i in collection:
+	A = line.split('-')		
+	for i in collection:	#Query by Roll number
 		roll_ = i[1].split('-')
 		if A[-1] == roll_[-1]:
 			attended.append(i)
 	
-	for i in collection:
+	for i in collection:	#Query by Full Name
 		name_ = i[0].upper()
 		if name_ == line.upper():
 			attended.append(i)		
 fh.close()
-#Writing Attendance
 
-#Switching sheet
-w_sh = wb[sheets[1]]
+#Function to Remove duplicate from attendance list
+def removeDuplicates(lst): 
+      return [t for t in (set(tuple(i) for i in lst))] 
+
+
+w_sh = wb[sheets[1]]	#Writing attendance in the Second Sheet
 
 i = 5
 
-while w_sh.cell(row=2, column=i).value != None:
-	i = i + 1
-	verify = w_sh.cell(row=2, column=i).value
-	current_date = datetime.today().strftime('%d-%m-%Y')
+
+while w_sh.cell(row=2, column=i).value != None:	#while date field is empty
+
+	i = i + 1		#Increament
 	
-	if  verify == None and verify != current_date:
+	verify = w_sh.cell(row=2, column=i).value			#Grabbing New date if available
+	current_date = datetime.today().strftime('%d-%m-%Y')	#Grabbing Current date from time library
+	
+	if  verify == None and verify != current_date:	#If today's attendance is not written
 		col_letter = get_column_letter(i)
 		index = col_letter + '2'
 		print(index)
 		w_sh[index] = current_date
 		wb.save('Attendance.xlsx')
 		break
+		
 	elif verify == current_date:
-		print('Attendance is Up to Date')
+		print('Attendance is Up to Date')			#Else
 		break
+
+
+col = index[:1]
+co = column_index_from_string(col)
+row = index[1:5]
+ro = int(row) + 1
+
+
+while w_sh.cell(row=ro, column = co).value == None:
 	
+	roll = w_sh.cell(row=ro, column=2).value	
+	name_ = w_sh.cell(row=ro, column=4).value
+	name = name_.strip()
+	index = col + str(ro)
+	
+	jora = (name, roll)
+	#print(jora)
+	if jora in  removeDuplicates(attended):
+		w_sh[index] = 'P'
+	
+	else:
+		w_sh[index] = ''
+		
+		
+	ro = ro + 1
 
-
-
-
-
-
+print(attended)
+wb.save('Attendance.xlsx')
+	
